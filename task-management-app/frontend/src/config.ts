90ada89b4bf35126d536e35f8b5e0eab63dbeb0a
Kartikeya <kartikeyaku@gmail.com>
2025-08-17 22:20:12 -0700

    Deleted frontend folder (1)

--- task-management-app/frontend/src/config.ts
+++
@@ -1,5 +0,0 @@
-// Configuration constants
-export const API_URL = typeof __API_URL__ !== 'undefined' ? __API_URL__ : 'http://localhost:5000';
-
-// Declare the webpack-injected constant
-declare const __API_URL__: string;
